49795b6417b0ae75de68c1a5abf1d5871f59a63d
barnaby.parker <barnaby.parker.general@gmx.com>
2019-08-29 23:28:36 +0100

    Implemented javascipt for driving google maps api.

--- GisExperimentSite/SiteServer/Controllers/HomeController.cs
+++ GisExperimentSite/SiteServer/Controllers/HomeController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using SiteServer.Models;
+
 
 namespace SiteServer.Controllers
 {
@@ -15,8 +18,21 @@ namespace SiteServer.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Log(string search, string firstHit)
         {
+            try
+            {
+                dynamic firstHitObject = JObject.Parse(firstHit);
+                JObject output = new JObject();
+                output.Add("searchTerm", search);
+                output.Add("firstResult", firstHitObject);
+                System.IO.File.AppendAllText("logfile.json", $"{output.ToString(Newtonsoft.Json.Formatting.None)}{Environment.NewLine}");
+                Response.StatusCode = (int)HttpStatusCode.OK;
+            }
+            catch
+            {
+                System.IO.File.AppendAllText("logfile.json", $"Bad Request {Environment.NewLine}");
+            }
             return View();
         }
 
